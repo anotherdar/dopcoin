f9e26862dfbe3d3646aa4d1a9f8d93996eb191dd
anotherdar <d.rosario@unphu.edu.do>
2021-05-13 21:01:55 -0400

    starter

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Dopcoin – La Mejor Opción para Comprar y Vender Activos Digitales.</title>
+      </Head>
+      
+      <div className="bg-gray-800 h-screen">
+        <h1>starter</h1>
+      </div>
+    </>
+  )
+}
